4d4a14fe61ab66e41428a78b44a8eb356957a897
Kas Galstaun <kgalstaun@fastmail.com>
2021-06-24 10:15:06 +0200

    First implementation homepage with rendered list

--- src/service/QueryService.js
+++ src/service/QueryService.js
@@ -1,11 +1,9 @@
-import Constant from "../constants/constants";
 import { request } from "graphql-request";
 
 const QueryService = {
-  theme: {},
   async fetch(query, param) {
     let data;
-    data = await request(Constant.get("global", "API_ENDPOINT"), query, param);
+    data = await request(process.env.VUE_APP_API_ENDPOINT, query, param);
     return data;
   },
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,51 @@
 <template>
-  <div class="about">
-    <span>This is the home page</span>
+  <div id="home" v-if="post">
+    <div v-html="post.intro.html" class="mt-md"></div>
+
+    <ul id="array-rendering">
+      <li
+        v-for="(item, index) in post.list"
+        :key="index"
+        class="flex flex-row mb-md"
+      >
+        <span class="w-1/3 pr-md">{{ item.title }}</span>
+        <span class="w-2/3">{{ item.text }}</span>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+import { ref, onMounted } from "vue";
+import QueryService from "@/service/QueryService";
+import PostQuery from "@/queries/post";
+
+export default {
+  setup() {
+    onMounted(() => fetchData());
+
+    let post = ref(null);
+
+    async function fetchData() {
+      let data = await QueryService.fetch(PostQuery, { title: "home" });
+      post.value = data.posts[0];
+    }
+
+    return {
+      post,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#home {
+  width: 100%;
+
+  ul li {
+    span:first-child {
+      font-weight: bold;
+    }
+  }
+}
+</style>
