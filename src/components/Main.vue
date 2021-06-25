7880d7c5f5f188a2f7a41cdf5cb25ad3034572f3
Kas Galstaun <kgalstaun@fastmail.com>
2021-06-25 16:07:54 +0200

    Add 'show' v-if clause to router-view

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,11 +2,28 @@
   <main class="w-full flex items-start">
     <router-view v-slot="{ Component }">
       <transition name="fade" mode="out-in">
-        <component :is="Component" :key="'a' + $route.path" />
+        <component v-if="show" :is="Component" :key="'a' + $route.path" />
       </transition>
     </router-view>
   </main>
 </template>
+
+<script>
+import { onMounted, ref } from "vue";
+
+export default {
+  setup() {
+    let show = ref(false);
+    onMounted(() => {
+      show.value = true;
+    });
+
+    return {
+      show,
+    };
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 .fade-enter-active {
